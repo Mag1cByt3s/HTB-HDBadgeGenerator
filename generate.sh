86ac4811022773132be6d62e47852c5eded5282e
Pascal3366 <ppeinecke@protonmail.com>
2023-05-04 07:23:44 +0200

    Update 878647.html

---
+++ generate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Change directory to the git repository
+cd /home/generator/HTB-HDBadgeGenerator && /usr/bin/python3 /home/generator/HTB-HDBadgeGenerator/HTBHDBadgeGenerator.py --htbid 878647
