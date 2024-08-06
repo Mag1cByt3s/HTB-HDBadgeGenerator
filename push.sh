8b655a645ef5abab0619b245026da40fd7e20172
Pascal3366 <ppeinecke@protonmail.com>
2024-08-06 03:01:01 +0200

    Update 878647.html

--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Change directory to the git repository
-cd /home/generator/HTB-HDBadgeGenerator && git add * && git commit --allow-empty -am "Update 878647.html" && git push -u origin main --force
+cd /home/generator/HTB-HDBadgeGenerator && git add * && git commit --allow-empty -am "Update 878647.html" && git push -u origin main
